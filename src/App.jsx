aa6881020c1f65f3ef7b7d1075da26f4bf91db2d
Raquel Laglaive <ra.862@hotmail.com>
2023-05-18 21:15:52 -0300

    PreEntrega1+Laglaive

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+//COMPONENTES
+import NavBar from './components/NavBar/NavBar';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <h1>
+        PELUCAN
+      </h1>
+        <ul>
+            <li><a href='www.google.com'>INICIO</a></li>
+            <li><a href='www.google.com'>SERVICIOS</a></li>
+            <li><a href='www.google.com'>PRODUCTOS</a></li>
+            <li><a href='www.google.com'>NOSOTROS</a></li>
+            <li><a href='www.google.com'>CONTACTO</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar;
